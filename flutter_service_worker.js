349fff463879dc7a54e36afa111398749f7198ee
Andy <60817686+AvaruusAnkka@users.noreply.github.com>
2022-01-30 17:18:59 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,8 +17,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a3e0d1fa7b84abfacebb71bbed840ee0",
-"/": "a3e0d1fa7b84abfacebb71bbed840ee0",
+"index.html": "577e84daa9714fe2c429a34f13b54e5a",
+"/": "577e84daa9714fe2c429a34f13b54e5a",
 "main.dart.js": "437b428a60a5a99381b409099b8b6883",
 "manifest.json": "83d533e5ab22a8928a684b90a19bb48d",
 "version.json": "389a6e6e6f34312743ca5f36467c84b5"
